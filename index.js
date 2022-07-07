13fc9bca41bd2cb14429dc5c7ca8fe7027cac752
John Niemiec <51012201+cronJohn@users.noreply.github.com>
2022-07-06 23:59:14 -0500

    Work in progress

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hey');
+const modeChanger = document.getElementById("mode");
+const userInput = document.getElementById("user-input")
+
+// On click, toggle the mode between game and checker
+modeChanger.addEventListener("click", e => {
+    e.target.innerText == "✅" ? e.target.innerText = "🕹️" : e.target.innerText = "✅";
+});
+
+// Change the border of the input based on its state
+userInput.addEventListener("input", e => {
+    e.target.value === "" ? e.target.style.borderBottom = "10px solid yellow" : e.target.style.borderBottom = "10px solid green";
+    console.log(e.target.value);
+});

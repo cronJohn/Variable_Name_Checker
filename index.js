542a37cb72e14f118ff10a62e319f0f78e10cee0
John Niemiec <51012201+cronJohn@users.noreply.github.com>
2022-07-11 23:59:34 -0500

    Added JS functionality for modes

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const modeChanger = document.getElementById("mode");
 const userInput = document.getElementById("user-input")
+var allModeDA = document.querySelectorAll("[data-mode]");
 
 // On click, toggle the mode between game and checker
 modeChanger.addEventListener("click", e => {
-    e.target.innerText == "✅" ? e.target.innerText = "🕹️" : e.target.innerText = "✅";
+    if (e.target.innerText == "✅") {
+        e.target.innerText = "🕹️";
+        allModeDA.forEach(element => {
+            if (element.classList.contains("mode-header")){
+                element.innerText = 'Game time!'
+            }
+            element.dataset.mode = 'game'
+        });
+
+    }
+    else {
+        e.target.innerText = "✅";
+        allModeDA.forEach(element => {
+            if (element.classList.contains("mode-header")) {
+                element.innerText = 'Variable checker'
+            }
+            element.dataset.mode = 'checker'
+        });
+    }
+
 });
 
 // Change the border of the input based on its state
